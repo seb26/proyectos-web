eaa685c9c3e48995ce7e636e6a6f0b6321839874
seb26 <931319+seb26@users.noreply.github.com>
2017-09-22 21:35:26 -0500

    bring up to date before i put lots of images in

--- borrador/script.js
+++ borrador/script.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
   $("body").addClass("js");
   
   // Use Unveil to dynamically load images with this class
-  $(".img-dload").unveil(200);
+  $(".load-dyn").unveil(200);
   
   
   /* For: index.html
@@ -15,7 +15,9 @@ $(document).ready(function() {
   
   $('.ii-icon').mousedown(function(e){ e.preventDefault(); }); // Prevent double-click selecting on the icon box
   var image_info_overlay = false;
-  $('.image .ii-icon').click( function() {
+  $('.ii-icon').click( function() {
+    // The overlay text (ii-body)
+    // is found just one element prior
     image_info_overlay = $(this).prev();
     $(image_info_overlay).toggle();
   });
@@ -41,6 +43,33 @@ $(document).ready(function() {
       return;
     }
   });
+  
+  
+  $('.subcat-wrapper').mousedown(function(e){ e.preventDefault(); }); // Prevent double-click selecting on the icon box
+  var subcat_list = false;
+  $('#subcat-toggle-button').click( function() {
+    subcat_list = $(this).next();
+    $(subcat_list).toggle();
+  });
+  $(window).click( function(e) {
+    if ( subcat_list ) {
+      // User has clicked on an Info bubble
+      clicked = $(e.target);
+      parents = clicked.parents();
+      if ( clicked.hasClass('subcat-toggle') ) {
+        // If you click the icon, or the infobox
+        return;
+      }
+      else {
+        // If you click anywhere else
+        $(subcat_list).hide();
+      }
+    }
+    else {
+      // User has not clicked on an Info bubble
+      return;
+    }
+  });
      
   
   
@@ -55,11 +84,22 @@ $(document).ready(function() {
       var theSegment = $('#' + segment);
       var os = theSegment.offset().top;
       var ht = theSegment.prev().prev().height();
-      if( scroll > os - ht ){
-          $('#nav-subcat-toggle-button').text(theSegment.text());
+      if( scroll > os - ht ) {
+        // Change the label
+        $('#subcat-toggle-button').text(theSegment.text());
+        // Remove the active class
+        $('.subcat-menu a').removeClass('item-active');
+        // Add the class again to just one item
+        $('#subcat-item-' + segment).addClass('item-active');
       }
     });
   });
+  $('.subcat-menu a').click(function(e) {
+    $(e.target).removeClass('item-active');
+    $(e.target).parent().hide();
+    clicked_subcat = $(e.target).data('subcat');
+    // $('#subcat-menu-' + clicked_subcat).addClass('item-active');
+  })
 
     
 });
